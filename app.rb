40b02bccfaf4b993846609dd080bd26b5f72de28
Sebastian Spier <blog@airness.de>
2011-10-20 22:45:07 +0200

    adding some tests

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'RMagick'
 require 'curb'
 
 #
-# Verbosity: spit out more messages when set 
+# Verbosity: spit out more messages when set
 VERBOSE=true
 
 #
@@ -27,46 +27,46 @@ end
 # --- RMagick extensions ------------------------------------------------------
 
 class Magick::Image
-  
+
   # scale image down, but never up.
   def scale_down(width, height, format)
     change_geometry("#{width}x#{height}") do |cols, rows, img|
       next if cols >= img.columns || rows >= img.rows
       img.resize!(cols, rows)
     end
-  
+
     self
   end
-  
+
   # scale image up to fill widthxheight image
   def fill(width, height, format)
     change_geometry("#{width}x#{height}^") do |cols, rows, img|
       next if cols >= img.columns || rows >= img.rows
       img.resize!(cols, rows)
     end
-    
+
     #
     # get scale factor: the image might not have been resized, because
     # it is smaller than what was requested already.
     scale = [self.columns.to_f / width, self.rows.to_f / height].min
-    
-    dst = Magick::Image.new(width * scale,height * scale)    
+
+    dst = Magick::Image.new(width * scale,height * scale)
     dst.composite(self, Magick::CenterGravity, Magick::OverCompositeOp)
   end
-  
+
   # scale image to fit into widthxheight image
   def fit(width, height, format)
     change_geometry("#{width}x#{height}") do |cols, rows, img|
       next if cols >= img.columns || rows >= img.rows
       img.resize!(cols, rows)
     end
-    
+
     #
     # get scale factor: the image might not have been resized, because
     # it is smaller than what was requested already.
     scale = [self.columns.to_f / width, self.rows.to_f / height].max
-    
-    # cut out 
+
+    # cut out
     dst = Magick::Image.new(width * scale,height * scale) {
       self.background_color = format == "png" ? "none" : "white"
     }
@@ -97,22 +97,22 @@ end
 
 get %r{/fit(/(jpg|png)([0-9]{1,3})?)?(/([0-9]{1,4})(/([0-9]{1,4}))?)/((http|https)://.*)} do
   dlog params[:captures].inspect
-  
+
   # get parameters
   _,format,quality,_,width,_,height,url,_ = *params[:captures]
-  process :fit, format, quality, width, height, url 
+  process :fit, format, quality, width, height, url
 end
 
 get %r{/fill(/(jpg|png)([0-9]{1,3})?)?(/([0-9]{1,4})(/([0-9]{1,4}))?)/((http|https)://.*)} do
   # get parameters
   _,format,quality,_,width,_,height,url,_ = *params[:captures]
-  process :fill, format, quality, width, height, url 
+  process :fill, format, quality, width, height, url
 end
 
 get %r{(/(jpg|png)([0-9]{1,3})?)?(/([0-9]{1,4})(/([0-9]{1,4}))?)/((http|https)://.*)} do
   # get parameters
   _,format,quality,_,width,_,height,url,_ = *params[:captures]
-  process :scale_down, format, quality, width, height, url 
+  process :scale_down, format, quality, width, height, url
 end
 
 # -----------------------------------------------------------------------------
@@ -126,7 +126,7 @@ helpers do
     'gif'  => 'image/gif',
     'png'  => 'image/png'
   }
-  
+
   def mime_type_for(format)
     MIME_TYPES[format] || "application/octet-stream"
   end
@@ -154,10 +154,10 @@ helpers do
     if height <= 0
       height = img.rows * width / img.columns
     end
-     
+
     # process image
     img = img.send mode, width.to_i, height.to_i, format
-    
+
     # write out image
     blob = img.to_blob do |img|
       img.format = format.upcase
@@ -174,7 +174,7 @@ helpers do
 
     blob
   end
-  
+
 end
 
 # -----------------------------------------------------------------------------
---
+++ tests/resizing_test.rb
@@ -0,0 +1,46 @@
+Bundler.require(:default, :test)
+
+require_relative '../app.rb'
+require 'test/unit'
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
+
+class ImgioTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_it_says_hello_world
+    get '/'
+    assert last_response.ok?
+    assert_equal 'Hello, world!', last_response.body
+  end
+
+  def test_it_fills_50_50
+    get '/fill/50/50/http://www.rubycgi.org/image/ruby_gtk_book_title.jpg'
+    img = ImageSize.new(last_response.body)
+    assert_equal 50, img.width
+    assert_equal 50, img.height
+  end
+
+  def test_it_fits_50_50
+    get '/fill/50/50/http://www.rubycgi.org/image/ruby_gtk_book_title.jpg'
+    img = ImageSize.new(last_response.body)
+    assert_equal 50, img.width or assert_equal 50, img.height
+  end
+
+  def test_it_returns_jpg
+    get '/fill/jpg/50/50/http://www.rubycgi.org/image/ruby_gtk_book_title.jpg'
+    img = ImageSize.new(last_response.body)
+    assert_equal :jpeg, img.format
+  end
+
+  def test_it_returns_png
+    get '/fill/png/50/50/http://www.rubycgi.org/image/ruby_gtk_book_title.jpg'
+    img = ImageSize.new(last_response.body)
+    assert_equal :png, img.format
+  end
+end
