efcdb08ae92d9043e219ac976b77875a0d49957b
eno <eno@open-lab.org>
2011-10-21 14:00:08 +0200

    Adjusted tests to not produce square images
    …because when testing against square images one might 
    just confuse width and height expectations.

--- tests/resizing_test.rb
+++ tests/resizing_test.rb
@@ -19,27 +19,27 @@ class ImgioTest < Test::Unit::TestCase
     assert_equal 'Hello, world!', last_response.body
   end
 
-  def test_it_fills_50_50
-    get '/fill/50/50/http://www.rubycgi.org/image/ruby_gtk_book_title.jpg'
+  def test_it_fills_100_50
+    get '/fill/100/50/http://www.rubycgi.org/image/ruby_gtk_book_title.jpg'
     img = ImageSize.new(last_response.body)
-    assert_equal 50, img.width
+    assert_equal 100, img.width
     assert_equal 50, img.height
   end
 
-  def test_it_fits_50_50
-    get '/fill/50/50/http://www.rubycgi.org/image/ruby_gtk_book_title.jpg'
+  def test_it_fits_100_50
+    get '/fill/100/50/http://www.rubycgi.org/image/ruby_gtk_book_title.jpg'
     img = ImageSize.new(last_response.body)
-    assert_equal 50, img.width or assert_equal 50, img.height
+    assert_equal 100, img.width or assert_equal 50, img.height
   end
 
   def test_it_returns_jpg
-    get '/fill/jpg/50/50/http://www.rubycgi.org/image/ruby_gtk_book_title.jpg'
+    get '/fill/jpg/100/50/http://www.rubycgi.org/image/ruby_gtk_book_title.jpg'
     img = ImageSize.new(last_response.body)
     assert_equal :jpeg, img.format
   end
 
   def test_it_returns_png
-    get '/fill/png/50/50/http://www.rubycgi.org/image/ruby_gtk_book_title.jpg'
+    get '/fill/png/100/50/http://www.rubycgi.org/image/ruby_gtk_book_title.jpg'
     img = ImageSize.new(last_response.body)
     assert_equal :png, img.format
   end
